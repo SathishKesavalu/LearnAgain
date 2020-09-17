ae284d5e263a1196ca04080cb436119bdcbdb8a1
sathish kesavalu <sathish.kesavalu@gmail.com>
2020-09-17 09:27:33 -0700

    third commit

--- Whatever/src/Javalearn.java
+++ Whatever/src/Javalearn.java
@@ -3,7 +3,7 @@ public class Javalearn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Nandhu");
+		System.out.println("Nandhu is a good girl");
 	}
  // For Pull Request
 }
